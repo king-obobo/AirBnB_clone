0071bf70eb5047ce127d43063056cdfde244b4da
king-obobo <oboboebuka1@gmail.com>
2023-03-26 07:07:48 -0700

    Wrote some tests

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py
+
+    Unittest classes:
+        TestBaseModel_instantiation
+        TestBaseModel_save
+        TestBaseModel_to_dict
+"""
+from models.base_model import BaseModel
+from datetime import datetime
+import os
+import unittest
+import sleep
+
+
+class TestBaseModel_instantiation(unittest.TestCase):
+    """Unittests for testing the instantiation of the BaseModel class"""
+
+    def test_no_args_instantiates(self):
+        self.assertEqual(BaseModel(), type(BaseModel()))
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_two_models_unique_ids(self):
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+
+    def test_two_models_different_created_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.created_at, bm2.created_at)
+
+    def test_two_models_different_updated_at(self):
+        bm1 = BaseModel()
+        sleep(0.05)
+        bm2 = BaseModel()
+        self.assertLess(bm1.updated_at, bm2.updated_at)
+
+    def test_str_representation(self):
+        dt = datetime.today()
+        dt_repr = repr(dt)
+        bm = BaseModel()
+        bm.id = "11111"
+        bm.created_at = bm.updated_at = dt
+        bmstr = bm.__str__()
+        self.assertIn("[BaseModel] (11111)", bmstr)
+        self.assertIn("'id': '11111'", bmstr)
+        self.assertIn("'created_at': " + dt_repr, bmstr)
+        self.assertIn("'updated_at': " + dt_repr, bmstr)
