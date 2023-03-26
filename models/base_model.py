f59e2b7e7f0075d4f35cac5ccd0b27eeab38ff48
king-obobo <oboboebuka1@gmail.com>
2023-03-26 06:19:28 -0700

    Started afresh, finished answering task 3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,40 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class"""
-
-import models
+"""Defines the BaseModel class."""
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes:"""
-    def __init__(self, *args, **kwargs):
-        """Initializes the BaseModel
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Initializes a new BaseModel
+
         Args:
-            *args: Unused
-            **kwargs (dict): Key/Value pairs of attributes.
+            id (string): Assigned when an instance is created
+            created_at: Date-time assigned witch current datw when instance is
+            created
+            update_at: Date-time assigned with the current datetime when
+            instance is created and updated everytime objects is changes
         """
         DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "update_at":
-                    self.__dict__[k] = datetime.strptime(v, DATE_FORMAT)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
-
     def save(self):
-        """Updates update_at with the current date and time"""
+        """updates "updated_at" with the current datetime"""
         self.update_at = datetime.today()
-        models.storage.new(self)
 
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values of __dict__ of
-        the instance
-        """
-        dict_copy = self.__dict__.copy()
-        dict_copy["created_at"] = datetime.today()
-        dict_copy["update_at"] = datetime.today()
-        dict_copy["__class__"] = self.__class__.__name__
-        return dict_copy
+        """returns a dictionary containing all keys/values of the instance"""
+        ret_dict = self.__dict__.copy()
+        ret_dict["created_at"] = self.created_at.isoformat()
+        ret_dict["updated_at"] = self.update_at.isoformat()
+        ret_dict["__class__"] = self.__class__.__name__
+        return ret_dict
 
     def __str__(self):
-        """
-        Prints str in the form [<class name>] (<self.id>) <self.__dict__>
-        """
-        class_name = dict.__class__.__name__
+        """Returns the string representation of the BaseModel instance"""
+        class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
